6558680c268d568972f61023899ddaf6a5b9ac86
Nicolas <nicolas.tan@le-campus-numerique.fr>
2021-08-03 12:00:01 +0200

    checkbox panier commencé

--- SECTION/catalogue.php
+++ SECTION/catalogue.php
@@ -1,42 +1,5 @@
-<h2>Ajouter un article</h2>
-
-<?php
-include ('ASIDE/addArticle.php');
-?>
-
-<h1>Le catalogue des bonnes affaires 
-    
-<?php 
-// if (isset($GET['prenom'])) {
-//     echo '<p>pour</p>';
-//     echo $GET['prenom'];
-// }    
-?>
-</h1>
-
-<p>C'est les soldes ! faites des bonnes affaires parmi chaussures, casquettes, t-shirts... </p>
-
-
-<h2>Articles du jour<link href="bonjour.php" rel="stylesheet"></h2>
-
-    
-
-<?php 
-// if (isset($_POST['search'])) {
-//     header('Location: index.php?page=search');
-// }
-//  else {
-    echo '<form action="index.php?page=search" id="formHeader" method="POST">
-    <p><label for="search">Rechercher <input type="search" name="search"></label></p>
-    <input type="submit">
-    </form>';
-    // }
-    
-    // <button>Search</button>
-?>
-
 <div id="articles">
-    <?php
+<?php
     $tab = [
         [
             "id" => 1,
@@ -66,18 +29,72 @@ include ('ASIDE/addArticle.php');
         
         echo '<form action="index.php?page=panier" method="POST">
             <input type="number" name="quantite" min="0" required="required" placeholder="Entrer une quantité"/>
-            <input name="id" value='.$article["id"].' type="hidden" >
+            <input name="id" value='.$article["id"].' type="hidden">';
+        
+        echo '</div>';        
+    }
+    echo '<fieldset>';
+    echo '<input type="checkbox" name="checkbox[]" value="chaussure">';
+    echo '<input type="checkbox" name="checkbox[]" value="casquette">';
+    echo '<input type="checkbox" name="checkbox[]" value="tshirt">';
+    echo '</fieldset>';
+    echo '<p><input type="submit" value="Valider" /></p>
+    </form>';
+?>
+</div>
+
+<h2>Le catalogue des bonnes affaires</h2>
+<p>C'est les soldes ! faites des bonnes affaires parmi chaussures, casquettes, t-shirts... </p>
+
+<h2>Articles du jour<link href="bonjour.php" rel="stylesheet"></h2>
+
+<div id="articles">
+<?php
+    $tab = [
+        [
+            "id" => 1,
+            "nom" => "chaussure",
+            "prix" => 70,
+            'photo' => '../images/chaussures.jpg'
+        ],
+        [
+            "id" => 2,
+            "nom" => "casquette",
+            "prix" => 20,
+            'photo' => '../images/casquette.jpg'
+        ],
+        [
+            "id" => 3,
+            "nom" => "tshirt",
+            "prix" => 40,
+            'photo' => '../images/tshirt.jpg'
+        ]
+    ];
+
+    foreach($tab as $article) {
+        echo '<div class="article">';
+        // méthode de postage
+        
+        echo $article["nom"]." ".$article["prix"]." € ".'<a href="index.php?page=article&article='.$article["id"].'">'.'<img src ="'.$article["photo"].'" width="100" height="100"/>'." ".'</a>';
+        
+        echo '<form action="index.php?page=panier" method="POST">
+            <input type="number" name="quantite" min="0" required="required" placeholder="Entrer une quantité"/>
+            <input name="id" value='.$article["id"].' type="hidden">
             <p><input type="submit" value="Valider" /></p>
         </form>';
         // echo '<input type="checkbox" name="checked" value="checked"/>';
         // echo input id hidden
         echo '</div>';        
     }
-    // echo '<form action="SECTION/panier.php">';
-    
-    // echo '</form>';
-    ?>
+?>
 </div>
 
+<h2>Rechercher un article</h2>
 
+<?php 
+    echo '<form id="formRecherche" action="index.php?page=search" method="POST">';
+    echo '<p><label for="search">Rechercher <input type="search" name="search"></label></p>
+    <input type="submit">';
+    echo '</form>';
+?>
 
--- SECTION/search.php
+++ SECTION/search.php
@@ -1,19 +1,13 @@
-    
 <h1>Résultat de la recherche</h1>
-
 
 <?php 
     if (isset($_POST['search'])) {
-        echo 'something';
         include ('SECTION/article.php');
-        // include ('ASIDE/searchAside.php');
-
-        
     } 
-        echo '<form action="index.php?page=search" id="formHeader" method="POST">
-        <p><label for="search">Rechercher <input type="search" name="search"></label></p><button>Search</button>
-        </form>';
-    
+
+    echo '<form action="index.php?page=search" id="formHeader" method="POST">
+    <p><label for="search">Rechercher <input type="search" name="search"></label></p><button>Search</button>
+    </form>';
 
 ?>
 
